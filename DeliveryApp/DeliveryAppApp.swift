4a9ee9fcc0eab5433e49cf2c5b0e8a2bdbe0b5e2
FDC.badz <fdc.bryananjo@gmail.com>
2024-04-03 14:44:36 +0800

    Initial Commit

---
+++ DeliveryApp/DeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppApp.swift
+//  DeliveryApp
+//
+//  Created by FDCI on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
